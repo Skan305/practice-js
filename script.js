343edb06a07707f456857a911f5cbd315263826c
skhan305 <usajjad028@gmail.com>
2024-06-14 22:20:36 +0500

    final add

---
+++ script.js
@@ -0,0 +1,57 @@
+// Chap 14 nested if
+
+// let x = 2;
+// let y = 2;
+
+// if(x>0){
+//     if(y > 0){
+//         console.log("Both x and y are positive")
+//     }
+//     else{
+//         console.log("x is positive but y is not")
+//     }
+// }
+
+// else{
+    //     console.log("x is not positive")
+    // }
+    
+
+// Chap 15 ARRAYS
+
+// let cars = [];
+// cars[0] = "Audi";
+// cars[1] = "Mercedes";
+// cars[2] = "BMW";
+// cars[3] = "Mehran";
+// let cars = ["Audi","Mercedes","BMW","Mehran"];
+
+// console.log(cars.indexOf("Audi"));
+
+//["Audi","Mercedes","BMW","Mehran"]
+
+// cars.pop();
+//["Audi","Mercedes","BMW"]
+
+// cars.push("Toyota","Honda");
+//["Audi","Mercedes","BMW","Toyota","Honda"]
+
+// cars.shift();
+//["Mercedes","BMW","Mehran"]
+
+
+// cars.unshift("Civic","Pruis")
+//["Civic","Pruis","Audi","Mercedes","BMW","Mehran"]
+
+
+
+// console.log(cars.length);
+
+
+
+// let arr1 = [1, 2, 3];
+// let arr2 = [4, 5, 6];
+
+// let arr3 = arr1.concat(arr2)
+
+// console.log(arr3)
